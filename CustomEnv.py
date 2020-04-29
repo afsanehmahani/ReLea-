c24661193bac96235634499c3fe91dba160925fc
afsanehmahani <63519765+afsanehmahani@users.noreply.github.com>
2020-04-29 17:30:07 +0430

    Update CustomEnv.py

--- CustomEnv.py
+++ CustomEnv.py
@@ -9,68 +9,77 @@ import pandas as pd
 import numpy as np
 import os
 
-class CustomEnv(gym.Env):
+class first_env(gym.Env):
   """Custom Environment that follows gym interface"""
   
 
 
-  def __init__(self, cycl):
-    super(CustomEnv, self).__init__()
+  def __init__(self):
+    super(first_env, self).__init__()
     
     print(os.getcwd())
     os.chdir('C:/Users/admin/Desktop/RL-Project')
     print(os.getcwd())
-    self.testcases = pd.read_csv('tc_data_paintcontrol.csv', error_bad_lines=False, sep=';')
+    self.testcases = pd.read_csv('tc_firstcycle.csv', error_bad_lines=False, sep=';')
     print(self.testcases)
     
-    self.cycle = cycl
-    N_DISCRETE_ACTIONS = 114
+    length_tc = np.size(self.testcases,0)
+    
+    cycle = 1
+    self.cycle = cycle
+    
+    self.tcs_current_CI = np.where(self.testcases.iloc[:,7]== self.cycle)[0]
+    
+    N_DISCRETE_ACTIONS = np.size(self.testcases,0)
+    
     max_duration = max(self.testcases.iloc[:,2])
     min_duration = min(self.testcases.iloc[:,2])
     max_verdict = max(self.testcases.iloc[:,6])
     min_verdict = min(self.testcases.iloc[:,6])
     
-    self.tcs_current_CI = np.where(self.testcases.iloc[:,7]==cycl)[0]
-    print(self.tcs_current_CI)
+    
     # Define action and observation space
     # They must be gym.spaces objects
     # Example when using discrete actions:
     self.action_space = spaces.Discrete(N_DISCRETE_ACTIONS)
     # Example for using image as input:
-    self.observation_space = spaces.Box(low=np.array([min_duration, min_verdict]), high=np.array([max_duration, max_verdict]), dtype=np.uint8)
+    self.observation_space = spaces.Box(low=np.array([min_duration, min_verdict]), high=np.array([max_duration, max_verdict]),  dtype=np.float32)
     
     print (self.observation_space)
     
-  def step(self, action):
+  def step(self, act):
       
-      length_action = np.size(action,0)
+      print(act)
+      
+      length_action = np.size(act,0)
       reward=np.zeros(length_action, dtype=int)
        
       for tc in range(length_action):
           
-          rank = action[tc]
+          rank = act[tc]
           
-          if self.testcases.iloc[self.tcs_current_CI[tc] , 6] == 0:
+          if self.testcases.iloc[tc , 6] == 0:
               
-              reward[tc] = (min((np.linalg.norm(rank)-1), -np.linalg.norm(self.testcases.iloc[self.tcs_current_CI[tc] , 2])))
+              reward[tc] = (min((np.linalg.norm(rank)-1), -np.linalg.norm(self.testcases.iloc[tc , 2])))
           else:
-              reward[tc] = (1-abs(np.linalg.norm(rank)-np.linalg.norm(self.testcases.iloc[self.tcs_current_CI[tc] , 2])))
+              reward[tc] = (1-abs(np.linalg.norm(rank)-np.linalg.norm(self.testcases.iloc[tc , 2])))
     
       R1 = sum(reward/np.size(reward,0))
     
-      current_obs = self.tcs_current_CI
-      next_cycle = self.cycle + 1
-      tcs_next_CI = np.where( self.testcases.iloc[:,7]==next_cycle)[0]
+      
+      
     
-      observation = ([self.testcases.iloc[tcs_next_CI,2], self.testcases.iloc[tcs_next_CI,6]])
+      obs = ([self.testcases.iloc[length_action,2], self.testcases.iloc[length_action,6]])
+     
     
-      return observation, R1
+      return obs, R1
 
   def reset(self):
+ 
       
-      reset_cycle = self.cycle
-      reset_CI = self.tcs_current_CI
-      observation = ([self.testcases.iloc[reset_CI,2], self.testcases.iloc[reset_CI,6]])
       
-      return observation  # reward, done, info can't be included
+      obs = np.array([self.testcases.iloc[0,2], self.testcases.iloc[0,6]])
+      
+      
+      return obs  # reward, done, info can't be included
   
